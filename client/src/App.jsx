5165213c1b7186e5f8e7da87914f93b1f4675a5f
Harsh Khandelwal <khandelwalharsh121@gmail.com>
2023-08-19 20:57:13 +0530

    seventh commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { Container } from '@mui/material';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import {  GoogleOAuthProvider  } from '@react-oauth/google';
 import Home from './components/Home/Home';
 import Navbar from './components/NavBar/Navbar';
 import Auth from './components/Auth/Auth';
 
 const App = () => (
   <BrowserRouter>
-    <Container maxWidth="lg">
-      <Navbar />
-      <Routes>
-        <Route path="/" exact element={<Home/>} />
-        <Route path="/auth" exact element={<Auth/>} />
-      </Routes>
-    </Container>
+     <GoogleOAuthProvider clientId="441732350372-gvjcd2i4bdfpfcghhapqds0715thnbhk.apps.googleusercontent.com">
+        <Container maxWidth="lg">
+            <Navbar />
+            <Routes>
+                <Route path="/" exact element={<Home/>} />
+                <Route path="/auth" exact element={<Auth/>} />
+            </Routes>
+        </Container>
+    </GoogleOAuthProvider>
   </BrowserRouter>
 );
 
--- client/src/components/Auth/Auth.jsx
+++ client/src/components/Auth/Auth.jsx
@@ -1,18 +1,28 @@
 import React ,{useState}from 'react'
-
 import { Avatar , Button , Paper , Typography , Container , Grid, TextField } from '@mui/material';
+import {  GoogleLogin } from '@react-oauth/google';
 import useStyles from './Styles';
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Input from "./Input";
+import Icon from './Icon';
 
 const Auth = () => {
-   const classes = useStyles();
+    const classes = useStyles();
     const [showPassword , setShowPassword] = useState(false);
+    const [isSignup, setIsSignUp] = useState(false);
 
-   const handleChange = () => {};
-   const handleSubmit = () => {};
-   const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
-   const isSignup = true;
+    const handleChange = () => {};
+    const handleSubmit = () => {};
+    const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
+    const switchMode = () => setIsSignUp((prevSignUp) => !prevSignUp);
+
+   const googleSuccess = (res) => {
+        console.log(res);
+   };
+   const googleFailure = () => {
+        console.log(`Google Sign In was UnSuccessful. Try Again Later`);
+   };
+  
     return (
         <Container component='main' maxWidth='xs' >
           <Paper style={classes.paper} elevation={3}>
@@ -36,9 +46,29 @@ const Auth = () => {
                 <Input name='password' label='password' handleChange={handleChange} type={showPassword?"text" :"password" } handleShowPassword = {handleShowPassword} />
                 { isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" /> }
                 </Grid>
+               
+
                 <Button type='submit' fullWidth variant='contained' color='primary' style={classes.submit} >
                     {isSignup ? "Sign Up" : 'Sign In'}
                 </Button>
+                    <GoogleLogin
+                        
+                        render={(renderProps) => (
+                        <Button style={classes.googleButton} color="primary" fullWidth onClick={renderProps.onClick} disabled={renderProps.disabled} startIcon={<Icon/>} variant="contained">
+                            Google Sign In
+                        </Button>
+                        )}
+                        onSuccess={googleSuccess}
+                        onFailure={googleFailure}
+                        cookiePolicy="single_host_origin"
+                    />
+                <Grid container justifyContent="flex-end">
+                    <Grid item>
+                        <Button onClick={switchMode}>
+                            {isSignup ? 'Already have an Account ? Sign In' : "Don't have an account ? Sign Up"}
+                        </Button>
+                    </Grid>
+                </Grid>
             </form>
           </Paper>
 
